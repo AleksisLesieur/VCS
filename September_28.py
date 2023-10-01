a21ca876c1805c84a825513b53ce703151c21555
Aleksis Jonas Lesieur <69734158+AleksisLesieur@users.noreply.github.com>
2023-10-01 18:07:21 +0300

    Rugsejo 28 dienos namu darbai

---
+++ September_28.py
@@ -0,0 +1,27 @@
+# 1. Gyvuno kintamieji
+
+pavadinimas = 'kate'
+amzius = 8
+kailio_spalva = 'ruda'
+svoris = 4
+
+print(f'Gyvūnas - {pavadinimas} ({amzius}m) turi {kailio_spalva} kailio spalvą ir sveria {svoris} kg.')
+
+# 2. zodzio apvertimas
+
+desserts = 'desserts'[::-1]
+print(desserts)
+
+# 3. Tikrinimas vertes (sita uzduotis man iki galo neliko aiski)
+
+needle = 'racecar'
+haystack = 'The racecar is fast, but can it outrun a palindrome?'
+
+print(haystack.find(needle))
+
+# 4. Kintamuju sudejimas
+
+bool_1 = True
+number_string = '43'
+
+print(int(bool_1) + int(number_string))
